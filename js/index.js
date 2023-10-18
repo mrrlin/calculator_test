a59d8b9d9d5d7c600b998f618af0a4d85788e131
Angelina <bunny.iblis@gmail.com>
2023-10-19 00:17:06 +0300

    tmp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log("Hi");
+const firstNumber = document.getElementById("first-number");
+const sectionNumber = document.getElementById("second-number");
+const mathOperation = document.getElementById("math-operation");
+const buttonResult = document.getElementById("button-result");
+const result = document.getElementById("result");
+
+
+// console.log(firstNumber.value);
