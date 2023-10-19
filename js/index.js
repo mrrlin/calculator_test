c5992634e194dc380c002c99e01b1a44b693a5cf
Angelina <bunny.iblis@gmail.com>
2023-10-19 23:19:31 +0300

    Added base operations

--- js/index.js
+++ js/index.js
@@ -1,24 +1,59 @@
-const firstNumber = document.getElementById('firstNumber');
-const secondNumber = document.getElementById('secondNumber');
+const numberOfFirstLabel = document.getElementById('firstNumber');
+const numberOfSecondLabel = document.getElementById('secondNumber');
 
-const mathOperations = document.getElementsByName('mathOperation');
-let mathOperationChecked = "";
+const labels = document.getElementsByClassName('form__math-operation');
 
 const buttonResult = document.getElementById('buttonResult');
 const result = document.getElementById('result');
 
+const mathOperations = document.getElementsByName('mathOperation');
+let mathOperationChecked = "";
+
+
+
+// const checked = document.querySelector('input[name="mathOperation"]:checked');
+
 //restricton for enter: only numbers
-firstNumber.addEventListener('input', () => {
-  firstNumber.value = firstNumber.value.replace(/\D/g, '');
+numberOfFirstLabel.addEventListener('input', () => {
+  numberOfFirstLabel.value = numberOfFirstLabel.value.replace(/\D/g, '');
 });
 
-buttonResult.addEventListener('click', () => {
-  //searching for the checked element
+function getResultOfMathOperation(operation) {
+  const firstNumber = parseFloat(numberOfFirstLabel.value);
+  const secondNumber = parseFloat(numberOfSecondLabel.value);
+  let resultOfOperation = 0;
+
+  switch(operation) {
+    case '+':
+      resultOfOperation = firstNumber + secondNumber;
+      console.log('+');
+      return resultOfOperation;
+    case '-':
+      resultOfOperation = firstNumber - secondNumber;
+      console.log('-');
+      return resultOfOperation;
+    case '*':
+      resultOfOperation = firstNumber * secondNumber;
+      console.log('*');
+      return resultOfOperation;
+    case '/':
+      resultOfOperation = firstNumber / secondNumber;
+      console.log('/');
+      return resultOfOperation;
+  }
+}
+
+buttonResult.addEventListener('click', (e) => {
+  getResultOfMathOperation();
   mathOperations.forEach(mathOperation => {
     if(mathOperation.checked) {
       mathOperationChecked = mathOperation.value;
-      console.log(mathOperation.value);
       return;
     }
   });
+
+  result.innerHTML = 'Результат: ' + getResultOfMathOperation(mathOperationChecked);
+
+
+  // console.log('sign is ' + mathOperationChecked);
 });
