5aea8def90a2f31d49ed3e0521f6cdff1948f28e
Angelina <bunny.iblis@gmail.com>
2023-10-19 15:06:12 +0300

    Added searching an operation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-const firstNumber = document.getElementById("first-number");
-const sectionNumber = document.getElementById("second-number");
-const mathOperation = document.getElementById("math-operation");
-const buttonResult = document.getElementById("button-result");
-const result = document.getElementById("result");
+const firstNumber = document.getElementById('firstNumber');
+const secondNumber = document.getElementById('secondNumber');
 
+const mathOperations = document.getElementsByName('mathOperation');
+let mathOperationChecked = "";
 
-// console.log(firstNumber.value);
+const buttonResult = document.getElementById('buttonResult');
+const result = document.getElementById('result');
+
+//restricton for enter: only numbers
+firstNumber.addEventListener('input', () => {
+  firstNumber.value = firstNumber.value.replace(/\D/g, '');
+});
+
+buttonResult.addEventListener('click', () => {
+  //searching for the checked element
+  mathOperations.forEach(mathOperation => {
+    if(mathOperation.checked) {
+      mathOperationChecked = mathOperation.value;
+      console.log(mathOperation.value);
+      return;
+    }
+  });
+});
