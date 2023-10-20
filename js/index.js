ceb06190ccadb74e3e114b9c2916849cf0ed4a78
Angelina <angelina.kulyaeva@yandex.ru>
2023-10-20 16:35:55 +0300

    added check for correctness of entered values

--- js/index.js
+++ js/index.js
@@ -7,18 +7,53 @@ const mathOperations = document.getElementsByName('mathOperation');
 let mathOperationChecked = "";
 
 const buttonResult = document.getElementById('buttonResult');
-const result = document.getElementById('result');
+const resultLabel = document.getElementById('result');
 const clearForm = document.getElementById('clear');
+
+let resultOfOperation = 0;
+
+function checkNumbers(input) {
+  const regex = /^[+-]?\d+(\.)?\d*$/g;
+  const isMatching = input.value.match(regex);
+  if (isMatching) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+numberOfFirstLabel.addEventListener('click', () => {
+  resultLabel.innerHTML = 'Результат: ';
+});
+
+numberOfSecondLabel.addEventListener('click', () => {
+  resultLabel.innerHTML = 'Результат: ';
+});
 
 //restricton for enter: only numbers
 numberOfFirstLabel.addEventListener('input', () => {
-  numberOfFirstLabel.value = numberOfFirstLabel.value.replace(/[^0-9]|-/g, '');
+  if (checkNumbers(numberOfFirstLabel)) {
+    if (buttonResult.hasAttribute('disabled')) {
+      buttonResult.disabled = false;
+    }
+    numberOfSecondLabel.addEventListener('input', () => {
+      if (checkNumbers(numberOfSecondLabel)) {
+        if (buttonResult.hasAttribute('disabled')) {
+          buttonResult.disabled = false;
+        }
+      } else {
+        buttonResult.disabled = true;
+      }
+    });
+  } else {
+    buttonResult.disabled = true;
+  }
+  // numberOfFirstLabel.value = numberOfFirstLabel.value.replace(/\D/g, '');
 });
 
 function getResultOfMathOperation(operation) {
   const firstNumber = parseFloat(numberOfFirstLabel.value);
   const secondNumber = parseFloat(numberOfSecondLabel.value);
-  let resultOfOperation = 0;
 
   if (firstNumber && secondNumber) {
     switch(operation) {
@@ -34,6 +69,7 @@ function getResultOfMathOperation(operation) {
       case '/':
         if(secondNumber !== 0) {
           resultOfOperation = firstNumber / secondNumber;
+          resultOfOperation = resultOfOperation.toFixed(13);
         } else {
           resultOfOperation = 'Мы тут на 0 не делим (:';
         }
@@ -43,21 +79,26 @@ function getResultOfMathOperation(operation) {
         return resultOfOperation;
     }
   } else {
-    resultOfOperation = 'Оба поля должны быть заполнены';
-    return resultOfOperation;
+    resultOfOperation = 'Оба поля должны быть заполнены корректными значениями';
   }
+
+  return resultOfOperation;
 }
 
 buttonResult.addEventListener('click', () => {
-  getResultOfMathOperation();
   mathOperations.forEach(mathOperation => {
     if(mathOperation.checked) {
       mathOperationChecked = mathOperation.value;
       return;
     }
   });
+  
+  let finishResult = getResultOfMathOperation(mathOperationChecked);
 
-  result.innerHTML = 'Результат: ' + getResultOfMathOperation(mathOperationChecked);
+  resultLabel.innerHTML = 'Результат: ' + finishResult;
 });
 
-clearForm.addEventListener('click', () => form.reset());
+clearForm.addEventListener('click', () => {
+  form.reset();
+  resultLabel.innerHTML = 'Результат: ' + 2;
+});
